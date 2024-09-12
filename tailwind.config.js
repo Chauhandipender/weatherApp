51bbc7a2738d408ef77ceaec7a2ce8bb8447f263
Chauhandipender <dipender0901@gmail.com>
2024-09-12 12:13:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#FFEB00',
+      },
+      fontFamily: {
+        poppins: ['poppins', 'system-ui'],
+      }
+    },
+  },
+  plugins: [],
+}
+
